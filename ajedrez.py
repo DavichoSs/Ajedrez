2a319814c10f2d14d30d80dffd2cb9eda32361d1
rubenpozo <ruben_oso28@hotmail.com>
2016-06-01 21:36:52 -0500

    llenado y pintado
    llena y pinta de cuadro en cuadro

--- ajedrez.py
+++ ajedrez.py
@@ -6,3 +6,16 @@ import time #permite pausar la aplicación al final por un timpo determinado
 vent=turtle.Screen()
 vent.bgcolor('brown') # color de fondo de la ventana
 vent.title('Tablero de Ajedrez') #titulo de la aplicación
+
+greg=turtle.Turtle()
+greg.speed(20) #velocidad con la que dibuja en el tablero
+
+#dibujar y llenar un cuadro
+def cuadro(tamaño,alternar,color):
+	greg.color(color)
+	greg.begin_fill()
+	for i in range(4):
+		greg.fd(tamaño)
+		greg.lt(90)
+	greg.end_fill()
+	greg.fd(tamaño)
