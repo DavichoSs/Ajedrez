2f014f958677d5fee89b5761a0ab80a291f644d6
rubenpozo <ruben_oso28@hotmail.com>
2016-06-01 21:38:28 -0500

    llenado completo- versión final
    llena de forma completa el tablero, con la opción de modificar el tamaño
    de los cuadros y la velocidad de dibujo

--- ajedrez.py
+++ ajedrez.py
@@ -19,3 +19,31 @@ def cuadro(tamaño,alternar,color):
 		greg.lt(90)
 	greg.end_fill()
 	greg.fd(tamaño)
+
+#dibujar una fila de cuadrados
+def fila(tamaño,alternar,color1,color2):
+	for i in range(4):
+		if(alternar==True):
+			cuadro(tamaño,alternar,color1)
+			cuadro(tamaño,alternar,color2)
+		else:
+			cuadro(tamaño,alternar,color2)
+			cuadro(tamaño,alternar,color1)
+			
+#Dibujar el tablero completo
+def tablero(tamaño,alternar,color1,color2):
+	greg.pu()
+	greg.goto(-(tamaño*4),(tamaño*4))
+	for i in range(8):
+		fila(tamaño,alternar,color1,color2)
+		greg.bk(tamaño*8)
+		greg.rt(90)
+		greg.fd(tamaño)
+		greg.lt(90)
+		if(alternar==True):
+			alternar=False
+		else:
+			alternar=True
+
+tablero(50,True,'black','white')
+time.sleep(10)
